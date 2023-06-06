a07d1a83a4e98d289bf34b9ea9dbf35fe09b3065
asif <asifmohammed@evalogical.com>
2023-06-06 19:14:12 +0530

    started js

---
+++ js/dataTypes.js
@@ -0,0 +1,6 @@
+let number = 10;
+let bigInt = 12345678912345678912345678969696n;
+const firstName = "lional";
+const lastName = `messi`;
+console.log(number, bigInt, firstName, lastName);
+console.log(bigInt + 1n);
---
+++ js/variable declaration.js
@@ -0,0 +1,12 @@
+let name = "messi";
+console.log(name);
+name = "ronaldo";
+console.log(name);
+
+if (true) {
+  var place = "kollam";
+}
+console.log(place);
+
+const number = 10;
+console.log(number);
